981d65399a367c4317f7c818cd2b761d616e1763
Anna <142659843+msgregorsamsa@users.noreply.github.com>
2023-12-15 09:54:46 +0100

    Added customer test
    För att se om git gör det den ska.

---
+++ Camp_Sleepaway_SOVA_A/Camp_Sleepaway_SOVA_A/Camper.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Camp_Sleepaway_SOVA_A
+{
+    public class Camper
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
