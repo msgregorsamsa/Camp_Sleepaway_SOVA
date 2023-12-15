c7cadb2b1b214bea12a0324e17099d2f8a43932c
veronicasteen <veronica.steen@outlook.com>
2023-12-15 10:07:11 +0100

    Person-klass

---
+++ Camp_Sleepaway_SOVA_A/Camp_Sleepaway_SOVA_A/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camp_Sleepaway_SOVA_A
+{
+    public class Person
+    {
+        public int Id { get; set; }
+    }
+}
