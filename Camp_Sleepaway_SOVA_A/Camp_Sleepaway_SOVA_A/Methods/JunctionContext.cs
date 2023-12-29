87aee15d2fceda4fffb3134dde14d8626fe850e2
Anna <142659843+msgregorsamsa@users.noreply.github.com>
2023-12-29 12:48:28 +0100

    Cleanup i JunctionContext

--- Camp_Sleepaway_SOVA_A/Camp_Sleepaway_SOVA_A/Methods/JunctionContext.cs
+++ Camp_Sleepaway_SOVA_A/Camp_Sleepaway_SOVA_A/Methods/JunctionContext.cs
@@ -1,34 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Camp_Sleepaway_SOVA.Methods
 {
     public class JunctionContext
     {
-        public static Cabin chooseCabin(CampContext context) //Ger oss ett menyval med en lista av alla befintliga cabins
+        //Skapar ett menyval med en lista av befintliga cabins.
+        //Användarens val returneras baserat på ID på val i listan och cabinName
+        public static Cabin chooseCabin(CampContext context)
         {
-            // Skapar ett object av Cabins innehåll
-            var cabins = context.Cabins;
-            // Gör en array av alla namnen
-            var cabinNames = cabins.Select(c => c.Name).ToArray();
-            // Få användaren att välja en av dem
-            var cabinChoice = Program.ShowMenu("Välj cabin:", cabinNames);
-            // Använd användarens val till att hitta rätt object, välj sedan ID från detta objektet och retunera bara första av dem (utifall vi har fler cabins som heter samma)
+            var cabins = context.Cabins; //Objekt av cabins innehåll
+
+            var cabinNames = cabins.Select(c => c.Name).ToArray(); //Till array
+
+            var cabinChoice = Program.ShowMenu("Välj cabin:", cabinNames); // användaren väljer m.h.a. showmenu
+
             return cabins.Where(c => c.Name == cabinNames[cabinChoice]).FirstOrDefault();
         }
 
+
+        //Skapar ett menyval med en lista av befintliga counselors.
+        //Användarens val returneras baserat på ID på val i listan och counselorTitles
         public static Counselor chooseCounselor(CampContext context) //Ger oss ett menyval med en lista av alla befintliga counselors
         {
-            // Skapar ett object av Cabins innehåll
-            var counselors = context.Counselors;
-            // Gör en array av alla namnen
-            var counselorTitles = counselors.Select(c => c.Title).ToArray();
-            // Få användaren att välja en av dem
-            var counselorChoice = Program.ShowMenu("Välj counselor:", counselorTitles);
-            // Använd användarens val till att hitta rätt object, välj sedan ID från detta objektet och retunera bara första av dem (utifall vi har fler cabins som heter samma)
+            var counselors = context.Counselors; // Objekt av counselors innehåll
+
+            var counselorTitles = counselors.Select(c => c.Title).ToArray(); // Till array
+
+            var counselorChoice = Program.ShowMenu("Välj counselor:", counselorTitles);//Användaren väljer m.h.a. showmenu
+
             return counselors.Where(c => c.Title == counselorTitles[counselorChoice]).FirstOrDefault();
         }
     }
